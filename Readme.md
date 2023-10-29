d80d1dee7df38667ebb8c3b34e1d7345f6b8c7c0
Zayver <zayber.re@gmail.com>
2023-10-28 19:13:53 -0500

    Add Api Controller and Readme.md

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Labotatorio #1 Arquitectura de software
+## .NET Core y ASP.NET MVC
+
+### Elaborado por
+
+- Mauren Rivera
+- Angie Castro
+- Santiago Zuñiga
+
+## Requisitos
+
+- Docker
+- Docker compose plugin versión 3
+- Puerto 8080 libre
+
+## Ejecución
+
+```sh
+# En el root del proyecto
+docker compose up -d 
+```
+Acceder mediante el navegador a http://localhost:8080
--- personapi-dotnet/personapi-dotnet/Program.cs
+++ personapi-dotnet/personapi-dotnet/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using personapi_dotnet.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,24 +7,33 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "personapi-dotnet", Version = "v1" });
+});
+
 builder.Services.AddDbContext<PersonaDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     
 }
 
-
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "personapi-dotnet");
+});
+
 
 app.UseRouting();
 
